1b480e038e9f50b144002076d3f6a335d28c5898
gnostertag <ostertag2000@gmail.com>
2023-05-10 14:56:33 -0700

    missing label import

--- src/pages/ApartmentNew.js
+++ src/pages/ApartmentNew.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Button, Col, Form, FormGroup, Input, Row } from "reactstrap"
+import { Button, Col, Form, FormGroup, Input, Row, Label } from "reactstrap"
 
 const ApartmentNew = ({ createApartment }) => {
   const navigate = useNavigate();

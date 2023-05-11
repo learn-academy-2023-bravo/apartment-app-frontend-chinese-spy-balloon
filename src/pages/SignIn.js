2a05bef075a12aa4cd8fe6650f6b999a97c9bcfb
Spencerj1297 <sajohnson1297@gmail.com>
2023-05-11 11:17:13 -0700

    Sign in page form and styles

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,13 +1,45 @@
-
-
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { Button, Col, Form, FormGroup, Input, Row, Label } from "reactstrap"
 
 const SignIn = () => {
-
-    return (
-        <>
-            <h1>Sign In page</h1>
-        </>
-    )
+  return (
+    <>
+      <Form className="sign-in-form">
+        <Col md={4}>
+          <div className="form-group-container">
+            <FormGroup>
+              <Label for="apartment-street"></Label>
+              <Input
+                id="apartment-street"
+                name="street"
+                placeholder="login email"
+                type="text"
+                // onChange={handleChange}
+                // value={newApartment.street}
+              />
+            </FormGroup>
+          </div>
+          <div className="form-group-container">
+            <FormGroup>
+              <Label for="apartment-street"></Label>
+              <Input
+                id="apartment-street"
+                name="street"
+                placeholder="please enter password"
+                type="text"
+                // onChange={handleChange}
+                // value={newApartment.street}
+              />
+            </FormGroup>
+          </div>
+          <div className="form-group-container">
+            <Button>sign in</Button>
+          </div>
+        </Col>
+      </Form>
+    </>
+  )
 }
 
 export default SignIn

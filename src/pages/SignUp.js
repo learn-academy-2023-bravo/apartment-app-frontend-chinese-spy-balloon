0704d8f27d384c42dca144511d2c54bee2e470c5
bjohnwell <brandonjohnwell@gmail.com>
2023-05-11 13:31:22 -0700

    user create fucntionality

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,47 +1,93 @@
-import { Button, Form, FormGroup, Label, Input, FormText, Row, Col } from 'reactstrap';
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  FormText,
+  Row,
+  Col,
+} from "reactstrap"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
-const SignUp = () => {
-  return (
-    <Form className='sign_up'>
-      <Row>
-        <Col md={6}>
-          <FormGroup>
-            <Label for="sign_up_name"></Label>
-            <Input id="sign_up_name" name="name" placeholder="Name" type="text" />
-          </FormGroup>
-        </Col>
-        <Col md={6}>
-          <FormGroup>
-            <Label for="exampleEmail"></Label>
-            <Input id="exampleEmail" name="email" placeholder="Email" type="email" />
-          </FormGroup>
-        </Col>
-      </Row>
-      <Row>
-        <Col md={6}>
-          <FormGroup>
-            <Label for="examplePassword"></Label>
-            <Input id="examplePassword" name="password" placeholder="password" type="password" />
-          </FormGroup>
-        </Col>
-        <Col md={6}>
-          <FormGroup>
-            <Label for="confirmPassword"></Label>
-            <Input id="confirmPassword" name="password" placeholder="confirm password" type="password" />
-          </FormGroup>
-        </Col>
-      </Row>
-      <div>
- 
+const SignUp =
+  ({ createUser }) => {
+    const navigate = useNavigate()
+    const [newUser, setNewUser] = useState({
+      name: "",
+      email: "",
+      password: "",
+    })
 
-  <Button>
-    Normal
-  </Button>
-  {' '}
+    const handleChange = (e) => {
+      setNewUser({ ...newUser, [e.target.name]: e.target.value })
+    }
+    const handleClick = () => {
+      createUser(newUser)
+      navigate("/signin")
+    }
+    return (
+      <Form className="sign_up">
+        <Row>
+          <Col md={6}>
+            <FormGroup>
+              <Label for="sign_up_name"></Label>
+              <Input
+                id="sign_up_name"
+                name="name"
+                placeholder="Name"
+                type="text"
+                onChange={handleChange}
+                value={newUser.name}
+              />
+            </FormGroup>
+          </Col>
+          <Col md={6}>
+            <FormGroup>
+              <Label for="exampleEmail"></Label>
+              <Input
+                id="exampleEmail"
+                name="email"
+                placeholder="Email"
+                type="email"
+                onChange={handleChange}
+                value={newUser.email}
+              />
+            </FormGroup>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={6}>
+            <FormGroup>
+              <Label for="examplePassword"></Label>
+              <Input
+                id="examplePassword"
+                name="password"
+                placeholder="password"
+                type="password"
+                onChange={handleChange}
+                value={newUser.password}
+              />
+            </FormGroup>
+          </Col>
+          <Col md={6}>
+            <FormGroup>
+              <Label for="confirmPassword"></Label>
+              <Input
+                id="confirmPassword"
+                name="confirPassword"
+                placeholder="confirm password"
+                type="password"
+              />
+            </FormGroup>
+          </Col>
+        </Row>
+        <div>
+          <Button onClick={handleClick}>Submit</Button>
+        </div>
+      </Form>
+    )
+  }
 
-</div>
-    </Form>
-  );
-};
-
-export default SignUp;
+export default SignUp
